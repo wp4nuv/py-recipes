bfd620bc1e334c605c769897bf0d9e5fa50806ee
Pedro Maldonado <pedro@bridgeloannetwork.com>
2019-09-29 17:22:12 -0400

    gitignore updated, initial migrations included

--- therecipes/models.py
+++ therecipes/models.py
@@ -1,13 +1,24 @@
 from django.db import models
 
-
 class Ingredient(models.Model):
     ingredient_name = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.ingredient_name
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.category_name
+
 class Recipe(models.Model):
     recipe_name = models.CharField(max_length=50)
+    recipe_category = models.ForeignKey(Category, on_delete=models.DO_NOTHING, blank=True, default=0)
     ingredients = models.ManyToManyField(Ingredient, through="RecipeIngredient")
     instructions = models.CharField(max_length=5000)
+ 
+    def __str__(self):
+        return self.recipe_name
 
 class RecipeIngredient(models.Model):
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
--- therecipes/views.py
+++ therecipes/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
+    # main page is for searching; text or category
     return HttpResponse("Hello, world. This is the index.")
